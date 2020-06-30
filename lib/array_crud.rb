7da9a904082e15e1867c0454d12f7ef8b23b5251
Leslie G <lesliegonzalez@me.com>
2020-06-30 18:20:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [Painted, Phats, Dida, Daisy]
+  [Painted, Phats, Dida, Daisy, Loofie]
 end
 
 def add_element_to_end_of_array(array, element)
