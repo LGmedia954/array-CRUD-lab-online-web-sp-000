f24a4cf09158988080cb60c1923e6ebf292ceffb
Leslie G <lesliegonzalez@me.com>
2020-06-30 18:21:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [Painted, Phats, Dida, Daisy, Loofie]
+  ferrets = [Painted, Phats, Dida, Daisy, Loofie]
 end
 
 def add_element_to_end_of_array(array, element)
