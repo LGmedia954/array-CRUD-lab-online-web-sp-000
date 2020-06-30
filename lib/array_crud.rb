18124c82a10c9ee7c866fe7d842e91d5aa64dd52
Leslie G <lesliegonzalez@me.com>
2020-06-30 19:01:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ferrets = ["Painted", "Phats", "Dida", "Daisy"]
+  ferrets = Array.new[]
   ferrets.push("Loofie")
 end
 
