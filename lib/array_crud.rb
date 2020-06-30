91ace71328d5f4ec81d7f8ecdee4cfcae0d93f28
Leslie G <lesliegonzalez@me.com>
2020-06-30 18:29:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(ferrets, "Loofie")
   ferrets = ["Painted", "Phats", "Dida", "Daisy"]
-  ferrets << "Loofie"
+  ferrets.push << "Loofie"
 end
 
 def add_element_to_start_of_array(array, element)
